a1f4fb7a217964fafc760af10827afe70e9067fe
FalseJin <2605701478@qq.com>
2025-04-10 23:05:21 +0800

    Config sport function database

--- app/src/main/java/com/example/healthapp/data/AppDatabase.kt
+++ app/src/main/java/com/example/healthapp/data/AppDatabase.kt
@@ -1,11 +1,38 @@
 package com.example.healthapp.data
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.healthapp.data.SportEntity
 
 
 
-@Database(entities = [MealEntity::class, FoodEntity::class], version = 2, exportSchema = false)
+@Database(entities = [MealEntity::class, FoodEntity::class, SportEntity::class],
+    version = 3,
+    exportSchema = false)
+
 abstract class AppDatabase : RoomDatabase() {
     abstract fun healthDao(): HealthDao
+    abstract fun sportDao(): SportDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "health_database"
+                )
+                    .fallbackToDestructiveMigration()  // 升级时自动清空数据库（测试时常用）
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
 }
